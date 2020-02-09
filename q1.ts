b3453323e9e870a9a43a27305c8b8abcb9078ad9
ivy <keivy1923@gmail.com>
2020-02-09 20:30:32 +0800

    日曆顯示完成 點擊事件待處理

--- q1.ts
+++ q1.ts
@@ -1,22 +1,45 @@
-let monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let weeks = ["S", "M", "T", "W", "T", "F", "S"];
 
-var d = new Date();
-var year = d.getFullYear();
-var month = d.getMonth();
-var today = d.getDate();
-var index = 0;
+    let monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let weeks = ["S", "M", "T", "W", "T", "F", "S"];
+    let yearInput = document.querySelector('#year');
 
-document.querySelector('#year').setAttribute("value", year.toString());
-
-monthes.forEach(element => {
-    creatMonthArea(index);
-    index++;
-});
-
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var today = d.getDate();
+    var mIndex = 0;
+    var wIndex = 0;
+    
+    yearInput.setAttribute("value", year.toString());
+    monthes.forEach(element => {
+        creatMonthArea(mIndex);
+        mIndex++;
+        
+    });
 
 function creatMonthArea(month){
-    createMonthHeader(month);
+    var monthDiv = createMonthHeader(month);
+    var firstDay  = getMonthFirstDay(year,mIndex);
+    var lastDay = getMonthLastDay(year,mIndex);
+    var days = getDaysInMonth(year,mIndex);
+    var dIndex = 6;
+    var dText = 0;
+    
+    for(var i =0 ; i <firstDay; i++){
+        dIndex++
+        createDay(mIndex,"&nbsp;", dIndex ,monthDiv);
+    }
+
+    for(var i = firstDay ; i < firstDay+days; i++){
+        dIndex++;
+        dText++;
+        createDay(mIndex,dText,dIndex,monthDiv);
+    }
+
+    for(var i = firstDay+days; i< 6*7 ; i++){
+        dIndex++
+        createDay(mIndex,"&nbsp;", dIndex ,monthDiv);
+    }
 }
 
 function createMonthHeader(month){
@@ -29,12 +52,13 @@ function createMonthHeader(month){
         h4.innerHTML = monthes[month];
         monthDiv.appendChild(h4);  
 
-    index = 0;
+    wIndex = 0;
     weeks.forEach(function(weekText){
-        var wd = createWeekHeader(index);
-        index++;
+        var wd = createWeekHeader(wIndex);
+        wIndex++;
         monthDiv.appendChild(wd);
     });
+    return monthDiv;
 }
 
 function createWeekHeader(week){
@@ -43,4 +67,31 @@ function createWeekHeader(week){
         hday.setAttribute("style","order:"+week);
         hday.innerHTML = weeks[week].toUpperCase();
         return hday;
+}
+
+function createDay(m,text,counter,monthDiv){
+    var  day = document.createElement("div");
+    if(m === month && text === today ) day.setAttribute("class","day today");
+    else day.setAttribute("class","day");
+    day.setAttribute("style","order:"+counter);
+    day.innerHTML = text;
+    monthDiv.appendChild(day);
+}
+
+function getMonthFirstDay(y,m){
+    return new Date(y,m,1).getDay();
+}
+
+function getMonthLastDay(y,m){
+    return new Date(y,(m+1),0).getDay();
+}
+
+function getDaysInMonth(y,m){
+    return new Date(y,(m+1),0).getDate();
+}
+
+function setYear(y){
+    
+    
+    this.year = y;
 }
